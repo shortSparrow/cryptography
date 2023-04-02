7f134b37bb3fca7663ba9f3be0c645c15129a109
Oleksandr Volkov <formobileprima@gmail.com>
2023-04-02 22:39:10 +0300

    integer factorization

---
+++ integerFactorization/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        IntegerFactorization integerFactorization = new IntegerFactorization();
+//        integerFactorization.factor(21); // [3,7]
+//        integerFactorization.factor(20); // [2,10] [4,5]
+        integerFactorization.factor(241099381); // [479,503339]
+    }
+}
